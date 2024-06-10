e9f9fa76016c7851899970841e73c14a8d82702b
noman-abid <noman.abid@azm.dev>
2024-06-10 16:37:27 +0500

    KC-VUE-09-removed-keycloak-adapter

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,23 +1,11 @@
 <template>
   <div class="User">
-    <h2>User {{ userName }}</h2>
-    <h2>Roles {{ userRoles?.join(" ") }}</h2>
-    <h2>Access Token {{ accessToken }}</h2>
-    <button @click="logOut">Log Out</button>
+    <h2>HomePage</h2>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import KeyCloakService from '../security/KeycloakService';
-
-const userName = ref(KeyCloakService.GetUserName());
-const accessToken = ref(KeyCloakService.GetAccesToken());
-const userRoles = ref(KeyCloakService.GetUserRoles());
-
-const logOut = () => {
-  KeyCloakService.CallLogOut();
-};
 </script>
 
 <style scoped>
